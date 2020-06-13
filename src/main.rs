8752c07a66b900849ef91e97be89c041c8a4e80f
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2020-06-12 23:21:41 -0400

    Implement basic API shape for desired app

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{App, HttpRequest, HttpServer, Responder};
+use actix_web_codegen::{get,post};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+#[get("/")]
+async fn control_panel(req: HttpRequest) -> impl Responder {
+    format!("TODO: Implement a control panel")
+}
+
+#[post("/fan_off")]
+async fn fan_off(req: HttpRequest) -> impl Responder {
+    format!("TODO: Call bottlerocket to turn off fan")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .service(control_panel)
+            .service(fan_off)
     })
     .bind("127.0.0.1:8088")?
     .run()
